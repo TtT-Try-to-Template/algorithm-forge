2e0eda5bfdb896b4765ca3399c7326ac9890a7be
west_east <west.east1832@gmail.com>
2025-01-08 15:59:26 +0900

    solved: java boj10808

--- west-eastH/src/Main.java
+++ west-eastH/src/Main.java
@@ -1,7 +1,25 @@
+import java.io.*;
+import java.util.*;
+
 public class Main {
+    static int[] arr = new int[30];
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
-        System.out.println("Hello, world!");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line = br.readLine();
+        StringTokenizer st = new StringTokenizer(line);
+        String word = st.nextToken();
+
+        for (int i = 0; i < word.length(); i++) {
+            arr[(int)word.charAt(i) - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 'a'; i <= 'z'; i++) {
+            sb.append(arr[i - 'a']).append(' ');
+        }
+
+        System.out.println(sb.toString());
     }
-
 }
