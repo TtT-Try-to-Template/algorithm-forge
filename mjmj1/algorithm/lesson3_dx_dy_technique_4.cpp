57f7fcdc73d871bcfc8dbc9d6b072753604bd37e
mjmj1 <mjong3365@gmail.com>
2025-01-10 22:34:05 +0900

    solved: Cpp CodeTree: Lesson3/dx dy technique 5

--- mjmj1/algorithm/lesson3_dx_dy_technique_4.cpp
+++ mjmj1/algorithm/lesson3_dx_dy_technique_4.cpp
@@ -2,51 +2,57 @@
 
 using namespace std;
 
-int n, m;
-int arr[100][100];
+int n, t;
+int r, c;
+char d;
 
 bool in_range(int x, int y)
 {
-    if ((x < m and x >= 0) and (y < n and y >= 0)) return true;
-
+    if ((x <= n and x > 0) and (y <= n and y > 0)) return true;
     return false;
+}
+
+int* get_direction(char dir)
+{
+    switch (dir)
+    {
+    case 'U':
+        return new int[2] { 1, 0 };
+    case 'D':
+        return new int[2] { -1, 0 };
+    case 'R':
+        return new int[2] { 0, 1 };
+    case 'L':
+        return new int[2] { 0, -1 };
+    }
 }
 
 int main()
 {
-    cin >> n >> m;
+    cin >> n >> t;
+    cin >> r >> c >> d;
 
-    int dy[4] = { 0, 1, 0, -1 }, dx[4] = { 1, 0, -1, 0 };
-    int x = 0, y = 0;
-    int dir_num = 0; // 0: 오른쪽, 1: 아래쪽, 2: 왼쪽, 3: 위쪽
+    int* dir = get_direction(d);
 
-    arr[y][x] = 1;
+    int x = c, y = r;
 
-    for (int i = 2; i <= n * m; i++)
+    for (int i = 0; i < t; i++)
     {
-        int nx = x + dx[dir_num], ny = y + dy[dir_num];
-        
-        if (!in_range(nx, ny) || arr[ny][nx] != 0)
+        int nx = x + dir[1], ny = y + dir[0];
+
+        if (in_range(nx, ny))
         {
-            dir_num = (dir_num + 1) % 4;
+            x = nx;
+            y = ny;
         }
-
-        x = x + dx[dir_num];
-        y = y + dy[dir_num];
-
-        arr[y][x] = i;
+        else
+        {
+            dir[0] = dir[0] * -1;
+            dir[1] = dir[1] * -1;
+        }
     }
 
-    for (int y = 0; y < n; y++)
-    {
-        for (int x = 0; x < m; x++)
-        {
-            if(arr[y][x] != 0)
-                printf("%d ", arr[y][x]);
-        }
-
-        printf("\n");
-    }
+    printf("%d %d", y, x);
 
     return 0;
 }
